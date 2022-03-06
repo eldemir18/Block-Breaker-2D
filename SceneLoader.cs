2daa8279ad03fabe535da41afc6db4aaa6ddf23b
Göksu Eldemir <55974337+eldemir18@users.noreply.github.com>
2022-03-06 14:08:27 +0300

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Start()
+    {
+        Screen.SetResolution(1440, 1080, true);
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        if(FindObjectOfType<GameSession>() != null)
+        {
+            FindObjectOfType<GameSession>().ResetGame();
+        }
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
